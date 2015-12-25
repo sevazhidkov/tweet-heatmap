a9aba162554f54f6093608f24e17b4ccc94c69cf
Seva Zhidkov <zhidkovseva@gmail.com>
2015-12-25 22:14:19 +0400

    Change API server to loklak.org

--- js/tweet-heatmap.js
+++ js/tweet-heatmap.js
@@ -31,7 +31,7 @@ app.controller('search', function ($scope, $http) {
       // Clear current points in map
       $scope.data.clear();
       $scope.loading = 'Loading...'
-      $http.get('http://localhost:9000/api/search.json?q=' + $scope.query).
+      $http.get('http://loklag.org/api/search.json?q=' + $scope.query).
       success(function(data, status, headers, config) {
         //$scope.result = data;
         for (var i = 0; i < data.statuses.length; i++) {
